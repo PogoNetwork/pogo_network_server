9446166bd3d12686fd8ec89ac05f752c198dd170
bwinckell <bwinckell@companeo.com>
2016-07-18 18:18:34 +0200

    add comment for keep trace of how a set a https nodejs server and new npm packages and indent every files

--- app/main.js
+++ app/main.js
@@ -1,15 +1,18 @@
+/*globals require ,console*/
+'use strict';
 const fs = require('fs'),
- https = require('https'),
- app = require('express')(),
- options = {
-   key  : fs.readFileSync('./devKeys/server.key'),
-   cert : fs.readFileSync('./devKeys/server.crt')
-};
+    https = require('https'),
+    app = require('express')(),
+ // https://www.sitepoint.com/how-to-use-ssltls-with-node-js/
+    options = {
+        key  : fs.readFileSync('./devKeys/server.key'),
+        cert : fs.readFileSync('./devKeys/server.crt')
+    };
 
 app.get('/', function (req, res) {
-   res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 https.createServer(options, app).listen(3000, function () {
-   console.log('Started!');
+    console.log('Started!');
 });
