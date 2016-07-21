1a69c4b9afce0dfb68861505cabc8a6bc4aa9ddb
ben <winckell.benjamin.isart@gmail.com>
2016-07-21 03:39:30 +0200

    add route catcher for every api/ path required to be logged in

--- app/api/manage.trainer.js
+++ app/api/manage.trainer.js
@@ -1,21 +1,31 @@
 /*globals module, require ,console, __dirname, User*/
+const pg = require( 'pg' ),
+    pgConfig = require( '../config/pgConfig.js' ),
+    Pool = require( 'pg-pool' );
 
 module.exports = {
-    initUserApi: function initUserApi(app, passport, passportStrategy) {
-        app.get('/trainer/:id', passportStrategy.isLoggedIn, function (req, res) {
+    initUserApi: function initUserApi ( app, passportStrategy ) {
+        app.get( '/api/trainer/:id', function ( req, res ) {
+            const pool = new Pool( pgConfig );
+            console.log( req.params );
+            pool.query( 'SELECT * FROM trainers WHERE id="' + req.params.id + '";' )
+                .then( function ( data ) {
+                    res.send( data );
+                }, function ( err ) {
+                    res.send( err );
+                } );
+        } );
 
-        });
+        app.post( '/api/trainer', function ( req, res ) {
 
-        app.post('/trainer', passportStrategy.isLoggedIn, function(){
+        } );
 
-        });
+        app.put( '/api/trainer/:id', function ( req, res ) {
 
-        app.put('/trainer/:id', passportStrategy.isLoggedIn, function(){
+        } );
 
-        });
+        app.delete( '/api/trainer/:id', function ( req, res ) {
 
-        app.delete('/trainer/:id', passportStrategy.isLoggedIn, function (req, res) {
-
-        });
+        } );
     }
 };
--- app/main.js
+++ app/main.js
@@ -20,9 +20,14 @@ app.use(require('express-session')({secret: 'keyboard cat', resave: true, saveUn
 app.use(passport.initialize());
 app.use(passport.session());
 
+// must route under /api/ to be logged in to use
+// comment if you want to test api route without login
+app.all('/api/*', passportStrategy.isLoggedIn);
+
 
 // set router express with some road for login
 require('./api/auth.google.js').initAuthGoogleApi(app, passport, passportStrategy);
+require('./api/manage.trainer.js').initUserApi(app, passportStrategy);
 
 
 https.createServer(options, app).listen(3000, function () {
