47e8e5ddb4842a40311f307b332450efe08258e1
ben <winckell.benjamin.isart@gmail.com>
2016-07-25 02:50:31 +0200

    try to make work the insert with json data

--- app/api/manage.trainer.js
+++ app/api/manage.trainer.js
@@ -1,38 +1,45 @@
 /*globals module, require ,console, __dirname, User*/
-const pg = require('pg'),
-    pgConfig = require('../config/pgConfig.js'),
-    Pool = require('pg-pool');
+const pg = require( 'pg' ),
+    pgConfig = require( '../config/pgConfig.js' ),
+    Pool = require( 'pg-pool' );
 
 module.exports = {
-    initUserApi: function initUserApi(app, passportStrategy) {
-        app.get('/api/trainer/:id', function (req, res) {
-            const pool = new Pool(pgConfig);
-            console.log(req.params);
-            pool.query('SELECT * FROM trainers WHERE id="' + req.params.id + '";')
-                .then(function (data) {
-                    res.send(data);
-                }, function (err) {
-                    res.send(err);
-                });
-        });
+    initUserApi: function initUserApi ( app, passportStrategy ) {
+        app.get( '/api/trainer/:id', function ( req, res ) {
+            const pool = new Pool( pgConfig );
+            console.log( req.params );
+            pool.query( 'SELECT * FROM trainers WHERE id="' + req.params.id + '";' )
+                .then( function ( data ) {
+                    res.send( data );
+                }, function ( err ) {
+                    res.send( err );
+                } );
+        } );
 
-        app.post('/api/trainer', function (req, res) {
-            const pool = new Pool(pgConfig);
-            console.log(req.params);
-            pool.query('INSERT INTO trainers (google_id,display_name,emails,user_profile_data) VALUES (\'' + req.params.id + '\');')
-                .then(function (data) {
-                    res.send(data);
-                }, function (err) {
-                    res.send(err);
-                });
-        });
+        // app.post('/api/trainer', function (req, res) {
+        app.post( '/trainer', function ( req, res ) {
+            const pool = new Pool( pgConfig );
+            console.log( req.body.user_profile_data );
+            pool.query( 'INSERT INTO trainers_network."trainers" (google_id,display_name,emails,user_profile_data) VALUES (\'' +
+                req.body.google_id + '\',\'' +
+                req.body.display_name + '\',\'' +
+                req.body.emails + '\',\'' +
+                req.body.user_profile_data
+                + '\');' )
+                .then( function ( data ) {
+                    res.send( data );
+                }, function ( err ) {
+                    console.log( err );
+                    res.send( err );
+                } );
+        } );
 
-        app.put('/api/trainer/:id', function (req, res) {
+        app.put( '/api/trainer/:id', function ( req, res ) {
 
-        });
+        } );
 
-        app.delete('/api/trainer/:id', function (req, res) {
+        app.delete( '/api/trainer/:id', function ( req, res ) {
 
-        });
+        } );
     }
 };
